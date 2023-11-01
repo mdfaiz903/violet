e6d515955728af80ebde5f1a1e813b4a52a64dbb
fayezmohammad096 <fayezmohammad096@gmail.com>
2023-11-01 21:27:06 +0600

    model & admin.py file added

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+admin.site.register(slider)
+admin.site.register(category)
+class categoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',)}
+admin.site.register(product)
+class productAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',)}
