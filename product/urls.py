d5631a15ef93e8ae29f47b3c35fb9fea63a87140
fayezmohammad096 <fayezmohammad096@gmail.com>
2023-11-19 18:19:39 +0600

    pagination component add dynamically

--- product/urls.py
+++ product/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('',index.as_view(),name='home'),
     path('product-detail/<str:slug>/',ProductDetails.as_view(),name='product-details'),
     path('category-detail/<str:slug>/',CategoryDetails.as_view(),name='category-details'),
-    path('Product-list',ProductList.as_view(),name='product-list'),
+    path('Product-list/',ProductList.as_view(),name='product-list'),
 ]
--- product/views.py
+++ product/views.py
@@ -2,6 +2,12 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView,ListView
 from . models import *
+from django.core.paginator import(
+     PageNotAnInteger,
+     EmptyPage,
+     InvalidPage,
+     Paginator,
+)
 # Create your views here.
 class index(TemplateView):
      template_name = "index.html"
@@ -38,12 +44,38 @@ class CategoryDetails(DetailView):
           context= super().get_context_data(**kwargs)
           context['products'] = self.get_object().products.all()
           return context
-     
+class customPaginator:
+     def __init__(self,request,queryset,paginated_by) -> None:
+          self.paginator = Paginator(queryset,paginated_by)
+          self.paginated_by = paginated_by
+          self.queryset = queryset
+          self.page = request.GET.get('page', 1)
+
+     def  get_queryset(self):
+          try:
+               queryset = self.paginator.page(self.page)
+          except PageNotAnInteger:
+               queryset = self.paginator.page(1) # when pageNotAnInteger defaulting to 1 if not present
+          except EmptyPage:
+               queryset = self.paginator.page(1)
+          except InvalidPage:
+               queryset = self.paginator.page(1)
+
+          return queryset
+          
 
 
 class ProductList(ListView):
      model = Product
      template_name='product/product_list.html'
      context_object_name = 'object_list'
+     paginate_by = 4 # How many item want to show
      def get_context_data(self, **kwargs):
-          return super().get_context_data(**kwargs)
+          
+          context =  super().get_context_data(**kwargs)
+          page_obj = customPaginator(self.request,self.get_queryset(),self.paginate_by)
+          queryset = page_obj.get_queryset()
+          Paginator = page_obj.paginator
+          context['object_list'] = queryset
+          context['paginator'] = Paginator
+          return context 
