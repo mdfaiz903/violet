7d2ae5355932c2ec55220433cb0b74fb10e48757
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-19 11:06:11 +0600

    Password Reset functionality added

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SESSION_COOKIE_AGE = 86400
 CART_ID = 'cart'
 COUPON_ID ='coupon' # add for coupon  varaiable name must all capital
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mfmamun359@gmail.com'
+EMAIL_HOST_PASSWORD = 'rqfg bljv rdgz wovg'
--- session/urls.py
+++ session/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from. views import loginuser,logoutUser,signup,pass_change
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 urlpatterns = [
     path('login/',loginuser,name='login'),
     path('logout/',logoutUser,name='logout'),
     path('signup/',signup,name='signup'),
     path('pass_change/',pass_change,name='pass_change'),
+    path('accounts/password_reset/ ',PasswordResetView.as_view(template_name='session/password_reset.html'),name='password_reset'),
+    path('accounts/password_reset/done/',PasswordResetDoneView.as_view(template_name='session/password_reset_done.html'),name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='session/password_reset_confirm.html'),name='password_reset_confirm'),
+    path('accounts/reset/done/',PasswordResetCompleteView.as_view(template_name='session/password_reset_complete.html'),name='password_reset_complete'),
 ]
+
+"""
+accounts/password_reset/ [name='password_reset']
+accounts/password_reset/done/ [name='password_reset_done']
+accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/reset/done/ [name='password_reset_complete']
+"""
