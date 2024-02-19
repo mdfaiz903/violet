03a8dfdf09e6085e9a13f9957116c9afafeb8616
fayezmohammad096 <fayezmohammad096@gmail.com>
2024-02-19 16:28:18 +0600

    User account confirmation throw email functionality added

--- session/urls.py
+++ session/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from. views import loginuser,logoutUser,signup,pass_change
+from. views import loginuser,logoutUser,signup,pass_change,activate
 from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
 urlpatterns = [
     path('login/',loginuser,name='login'),
     path('logout/',logoutUser,name='logout'),
     path('signup/',signup,name='signup'),
     path('pass_change/',pass_change,name='pass_change'),
+    path('activate/<uidb64>/<token>/',activate,name='activate'),
+
+
     path('accounts/password_reset/ ',PasswordResetView.as_view(template_name='session/password_reset.html'),name='password_reset'),
     path('accounts/password_reset/done/',PasswordResetDoneView.as_view(template_name='session/password_reset_done.html'),name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='session/password_reset_confirm.html'),name='password_reset_confirm'),
--- session/views.py
+++ session/views.py
@@ -1,10 +1,20 @@
 import  copy
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash,get_user_model
 from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm
 from django.contrib import messages
 from .forms import CustomSignupForm
 from cart . cart import Cart
+
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+
+User_model = get_user_model()
 # Create your views here.
 
 def loginuser(request):
@@ -41,13 +51,40 @@ def signup(request):
     if request.method == 'POST':
         form = CustomSignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request,'Registration Success')
+            user=form.save(commit=False)
+            user.is_active=False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = "Active your Account"
+            message = render_to_string('session/account.html',{
+                'user':user,
+                'domain':current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            send_mail = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject,message,to=[send_mail])
+            email.send()
+            messages.info(request,'Successfully created account, Please check your Mail box')
             return  redirect('login')
     else:
         form = CustomSignupForm()
     return  render(request,'session/signup.html',{'form':form})
 
+def activate(request,uidb64,token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User_model._default_manager.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,User_model.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active=True
+        user.save()
+        messages.success(request,'Your account is activate now,you cant now login')
+        return redirect('login')
+    else:
+        messages.warning(request,'Activation link is Invalid')
+        return redirect('signup')
 
 def pass_change(request):
     if request.user.is_authenticated:
