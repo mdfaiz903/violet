51aaea226b77f9fcc4af63ea3b5a832a641e0856
Mohammad  Faiz <50353454+mdfaiz903@users.noreply.github.com>
2024-04-17 23:34:07 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mfmamun359@gmail.com'
-EMAIL_HOST_PASSWORD = 'rqfg bljv rdgz wovg'
+EMAIL_HOST_PASSWORD = 'Your_password'
